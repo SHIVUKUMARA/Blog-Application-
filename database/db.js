a019f30471e0ee4ffaa7b733981d35a7c8fa5e9b
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-10 09:37:14 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const Connection = async (username, password) => {
     const URL = `mongodb+srv://${username}:${password}@cluster0.pedjj5f.mongodb.net/blogapp?retryWrites=true&w=majority`;
