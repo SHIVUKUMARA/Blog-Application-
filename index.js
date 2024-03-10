a0785e2b650d56296d580ff0438f3c32bc2affb0
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-10 10:37:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { join, dirname } from 'path';
+
 
 //components
 import Connection from './database/db.js';
